ae79921a83ee60236504d31348943b515fd2332e
manojkumar9292 <44526724+manojkumar9292@users.noreply.github.com>
2020-04-05 03:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app.secret_key='super-secret-key'
 #)
 #mail = Mail(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sql9330142:sInQaajpWF@sql9.freemysqlhosting.net/sql9330142'
+#replace below uri with prod uri
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/ccblog'
 db = SQLAlchemy(app)
 
 
